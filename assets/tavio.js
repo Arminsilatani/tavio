368c017e1f93971eef1354c40cd699d95e52eada
Arminsilatani <eynakii@gmail.com>
2026-06-24 21:53:35 +0330

    auto 2026-06-24 21:53:35

--- assets/tavio.js
+++ assets/tavio.js
@@ -9,7 +9,7 @@
 
 /* :::::::::::::::::::::::::: SUPABASE CLIENT :::::::::::::::::::::::::: */
 const SUPABASE_URL = 'https://vzqicidepdmraygulrey.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_kqRWgOmLISOE2EuLL1s8fw_NW6FJRTI';
+const SUPABASE_ANON_KEY = 'sb_publishable_kqRWgOmLISOE2EuLL1s8fw_WN6FJRTI';
 const { createClient } = supabase;
 const sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
